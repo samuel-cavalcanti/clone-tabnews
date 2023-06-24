1233c5162618372d09195c73db8096adb29b43fd
Samuel Cavalcanti <scavalcanti111@gmail.com>
2023-06-24 23:57:20 +0000

    formatado todos os documentos com Prettier

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 function Home(params) {
-   return <h1> A vida é a arte do encontro, embora haja tanto desencontro pela vida. </h1>; 
+  return (
+    <h1>
+      A vida é a arte do encontro, embora haja tanto desencontro pela vida.
+    </h1>
+  );
 }
-
 
 export default Home;
