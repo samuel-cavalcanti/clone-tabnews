8e2e257029e5cae4511650fe2de2ecd62352814f
samuel-cavalcanti <scavalcanti111@gmail.com>
2023-07-28 11:30:25 -0300

    adicionado página em construção

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
 function Home(params) {
   return (
-    <h1>
-      A vida é a arte do encontro, embora haja tanto desencontro pela vida.
-    </h1>
+   <div style={{
+    display:'flex',
+    'flex-direction':'column',
+    'align-items':'center',
+    margin: '1em',
+   }}>
+       <h1>
+         A vida é a arte do encontro, embora haja tanto desencontro pela vida.
+       </h1>
+       <p style={{
+            'font-size':'18px',
+            'margin-inline': '5em',
+       }}>
+    Pensando <strong>modo Tabnews</strong>. Esse pedacinho de internet deverá possuir todos conteúdos de valor concretos feitos por mim. Assim como o Tabnews avalia o valor do conteúdo pela sua capacidade de aglutinar pessoas em seu entorno. Os conteúdos postados neste pedacinho, são avaliados no quanto esse conhecimento me conectou com diferentes pessoas</p>
+
+   </div>
   );
 }
 
