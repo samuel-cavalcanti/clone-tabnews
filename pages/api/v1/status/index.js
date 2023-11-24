2ced624380e7fe7641f8b483f1f471aafbb49cc1
samuel-cavalcanti <scavalcanti111@gmail.com>
2023-11-24 10:37:19 -0300

    added `jsconfig.json`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,7 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 export default async function status(request, response) {
   const result = await database.query("SELECT 1+1 as sum;");
-  console.log("result: ",result);
+  console.log("result: ", result.rows);
   response.status(200).json({ chave: "está funcionando!" });
 }
