ecec99a06017fdf80c268b4a13af5a5ed677738f
samuel-cavalcanti <scavalcanti111@gmail.com>
2024-02-01 20:14:42 -0300

    running vercel

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,6 +3,7 @@ import database from "infra/database.js";
 export default async function status(_request, response) {
   const settings = await database.settings();
   const updatedAt = new Date();
+
   response.status(200).json({
     updated_at: updatedAt.toISOString(),
     dependencies: {
