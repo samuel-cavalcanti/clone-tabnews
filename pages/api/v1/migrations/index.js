68cf08cd0a3d437e0b2b8610920a8cdb45af9ed2
samuel-cavalcanti <scavalcanti111@gmail.com>
2025-06-16 08:26:19 -0300

    refactor: create migrator model

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,22 +1,11 @@
-import pgMigrate from "node-pg-migrate";
-import { resolve } from "node:path";
-import database from "infra/database";
 import { createDefaultRouter } from "infra/router";
+import { runMigrations } from "models/migrator";
 
 async function migrations(request, response) {
   const isDryRun = { POST: false, GET: true };
   const dryRun = isDryRun[request.method];
 
-  const migrations = await database.withClient((client) => {
-    return pgMigrate({
-      dbClient: client,
-      dryRun,
-      dir: resolve("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    });
-  });
+  const migrations = await runMigrations(dryRun);
 
   const status = migrations.length > 0 && !dryRun ? 201 : 200;
 
